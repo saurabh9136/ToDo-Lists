efaba1d3541bb177ee65c5c5536d4dec9063a4a5
Saurabh <saurabhgiri2411@gmail.com>
2023-04-30 19:18:14 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
 const routes: Routes = [
   {
@@ -13,6 +15,16 @@ const routes: Routes = [
   {
     path:"add-todo",
     component:AddTodoComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"home",
+    component:HomeComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"",
+    component:TodosComponent,
     pathMatch:"full"
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './MyComponents/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
